43e37da40398d0331a1b8e4fa9d4bbd8cfad79fc
hexianglong <736626079@qq.com>
2018-12-15 22:15:53 +0800

    修改main函数

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ if __name__ == "__main__":
     while 1:
         flag = 0
         print("* 1:填工时\n* 2:填餐票\n* 0:退出")
-        flag = int(input("输入参数："))
+        try:
+            flag = int(input("输入参数："))
+        except Exception:
+            print("参数类型错误!\n")
+            time.sleep(0.5)
+            continue
         if flag == 0:
             print("退出中...")
-            time.sleep(2)
+            time.sleep(1)
             break
         if flag == 1:
             add_times.add_times()

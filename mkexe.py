86bf76a7bd6f8669a149d8ab432410ee55e29801
hexianglong <736626079@qq.com>
2018-12-16 12:22:56 +0800

    修改配置脚本

--- mkexe.py
+++ mkexe.py
@@ -2,11 +2,17 @@ import os
 path = os.path.split(os.path.realpath(__file__))[0]
 
 req_path = path+"/requirements.txt"
-ico_path = path + "/ico/p2.ico"
-main_path = path + "/main/main.py"
 
 pkg_ins_cmd = "pip install -r " + req_path
+
+ico_path = path + "/ico/p2.ico"
+main_path = path + "/main/main.py"
 main_pyins_cmd = "pyinstaller -F -i " + ico_path + " " + main_path
+
+ico_path = path + "/ico/p1.ico"
+main_path = path + "/main/main_gui.py"
+main_gui_pyins_cmd = "pyinstaller -F -w -i " + ico_path + " " + main_path
 
 os.system(pkg_ins_cmd)
 os.system(main_pyins_cmd)
+os.system(main_gui_pyins_cmd)
